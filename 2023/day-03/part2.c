47874cb8059fed5cfc55fb644b2e4f73ef587624
fabiooo4 <fabioirimie@gmail.com>
2023-12-04 21:00:01 +0100

    added day-03 part2

---
+++ 2023/day-03/part2.c
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LINE_LENGTH 15
+#define FILE_LENGTH 12
+
+int isDigit(char c) {
+  if (c >= '0' && c <= '9') {
+    return 1;
+  }
+  return 0;
+}
+
+int main() {
+  FILE *input = fopen("test.txt", "r"); // in test.txt part1: 413 part2: 6756
+  char line[FILE_LENGTH][LINE_LENGTH];
+  int finalAnswer = 0;
+
+  // File check
+  if (input == NULL) {
+    printf("File couldn't open\n");
+    return 0;
+  }
+
+  int lineIdx = 0;
+  while (fgets(line[lineIdx], LINE_LENGTH, input)) {
+    lineIdx++;
+  }
+
+  for (int i = 0; i < FILE_LENGTH; i++) {
+    int partsMult = 1;
+
+    int parts;
+    for (int j = 0; line[i][j] != '\n'; j++) {
+      // If current character is a symbol print it
+      if (line[i][j] == '*') {
+        parts = 0;
+
+        // Count parts
+        // Left
+        if (isDigit(line[i][j - 1])) {
+          int tmp = 0;
+          int offset = 1;
+          parts++;
+
+          while (isDigit(line[i][j - offset])) {
+            tmp = atoi(&line[i][j - offset]);
+            offset++;
+          }
+          partsMult *= tmp;
+        }
+
+        // Right
+        if (isDigit(line[i][j + 1])) {
+          parts++;
+          partsMult *= atoi(&line[i][j + 1]);
+        }
+
+        // Top
+        if (isDigit(line[i - 1][j])) {
+          int tmp = 0;
+          int offset = 0;
+          parts++;
+
+          while (isDigit(line[i - 1][j - offset])) {
+            tmp = atoi(&line[i - 1][j - offset]);
+            offset++;
+          }
+          partsMult *= tmp;
+        }
+
+        if (!isDigit(line[i - 1][j]) && isDigit(line[i - 1][j - 1])) {
+          int tmp = 0;
+          int offset = 1;
+          parts++;
+
+          while (isDigit(line[i - 1][j - offset])) {
+            tmp = atoi(&line[i - 1][j - offset]);
+            offset++;
+          }
+          partsMult *= tmp;
+        }
+        if (!isDigit(line[i - 1][j]) && isDigit(line[i - 1][j + 1])) {
+          parts++;
+          partsMult *= atoi(&line[i - 1][j + 1]);
+        }
+
+        // Bottom
+        if (isDigit(line[i + 1][j])) {
+          int tmp = 0;
+          int offset = 0;
+          parts++;
+
+          while (isDigit(line[i + 1][j - offset])) {
+            tmp = atoi(&line[i + 1][j - offset]);
+            offset++;
+          }
+          printf("%d\n", tmp);
+          partsMult *= tmp;
+        }
+        if (!isDigit(line[i + 1][j]) && isDigit(line[i + 1][j - 1])) {
+          int tmp = 0;
+          int offset = 1;
+          parts++;
+
+          while (isDigit(line[i + 1][j - offset])) {
+            tmp = atoi(&line[i + 1][j - offset]);
+            offset++;
+          }
+          printf("%d\n", tmp);
+          partsMult *= tmp;
+        }
+        if (!isDigit(line[i + 1][j]) && isDigit(line[i + 1][j + 1])) {
+          parts++;
+          partsMult *= atoi(&line[i + 1][j + 1]);
+        }
+
+        if (parts == 2) {
+          finalAnswer += partsMult;
+          printf("%d %d\n", parts, partsMult);
+        } else {
+          partsMult = 0;
+        }
+      }
+    }
+  }
+  printf("Final answer: %d\n", finalAnswer);
+
+  fclose(input);
+}
