7954d3c2e74396f0b747d41086cc1a41b4431ec3
fabiooo4 <fabioirimie@gmail.com>
2023-12-04 18:22:01 +0100

    completed day-03 part1

--- 2023/day-03/part1.c
+++ 2023/day-03/part1.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define LINE_LENGTH 14
-#define FILE_LENGTH 12
+#define LINE_LENGTH 142
+#define FILE_LENGTH 140
 
 int isDigit(char c) {
   if (c >= '0' && c <= '9') {
@@ -12,7 +12,7 @@ int isDigit(char c) {
 }
 
 int main() {
-  FILE *input = fopen("test.txt", "r"); // in test.txt part1: 413 part2: 6756
+  FILE *input = fopen("input.txt", "r"); // in test.txt part1: 413 part2: 6756
   char line[FILE_LENGTH][LINE_LENGTH];
   int finalAnswer = 0;
 
@@ -32,6 +32,8 @@ int main() {
     int topSum = 0;
     int bottomSum = 0;
 
+    int leftSum = 0;
+
     int top = 0;
     int topLeft = 0;
     int topRight = 0;
@@ -39,7 +41,8 @@ int main() {
     int bottom = 0;
     int bottomLeft = 0;
     int bottomRight = 0;
-    for (int j = 0; j < LINE_LENGTH; j++) {
+
+    for (int j = 0; line[i][j] != '\n'; j++) {
       // If current character is a symbol print it
       if (!isDigit(line[i][j]) && line[i][j] != '.' && line[i][j] != '\n') {
 
@@ -47,65 +50,75 @@ int main() {
         // it to the sum
         int offset = 1;
         while (isDigit(line[i][j - offset])) {
-          horizontalSum = atoi(&line[i][j - offset]);
+          leftSum = atoi(&line[i][j - offset]);
           offset++;
         }
+        horizontalSum += leftSum;
+        leftSum = 0;
         if (isDigit(line[i][j + 1])) {
           horizontalSum += atoi(&line[i][j + 1]);
         }
 
         // Top
-        offset = 0;
-        while (i != 0 && isDigit(line[i - 1][j - offset])) {
-          top = atoi(&line[i - 1][j - offset]);
-          offset++;
-        }
+        if (isDigit(line[i - 1][j])) {
+          int offset = 0;
+          while (isDigit(line[i - 1][j - offset])) {
+            top = atoi(&line[i - 1][j - offset]);
+            offset++;
+          }
+          topSum += top;
+          top = 0;
+        } else {
+          // Top left
+          int offset = 1;
+          while (isDigit(line[i - 1][j - offset])) {
+            topLeft = atoi(&line[i - 1][j - offset]);
+            offset++;
+          }
+          topSum += topLeft;
+          topLeft = 0;
 
-        // Top left
-        offset = 0;
-        while (i != 0 && !isDigit(line[i - 1][j]) &&
-               isDigit(line[i - 1][j - offset - 1])) {
-          topLeft = atoi(&line[i - 1][j - offset - 1]);
-          offset++;
-        }
-        top += topLeft;
-        topLeft = 0;
-
-        // Top right
-        offset = 0;
-        if (i != 0 && !isDigit(line[i - 1][j]) &&
-            isDigit(line[i - 1][j + offset + 1])) {
-          topRight = atoi(&line[i - 1][j + offset + 1]);
-          offset++;
+          // Top right
+          offset = 1;
+          if (isDigit(line[i - 1][j + offset])) {
+            topRight += atoi(&line[i - 1][j + offset]);
+            offset++;
+          }
         }
 
         // Bottom
-        offset = 0;
-        while (i != LINE_LENGTH - 1 && isDigit(line[i + 1][j - offset])) {
-          bottom = atoi(&line[i + 1][j - offset]);
-          offset++;
-        }
+        if (isDigit(line[i + 1][j])) {
+          int offset = 0;
+          while (isDigit(line[i + 1][j - offset])) {
+            bottom = atoi(&line[i + 1][j - offset]);
+            offset++;
+          }
+          bottomSum += bottom;
+          bottom = 0;
+        } else {
+          // Bottom left
+          int offset = 1;
+          while (isDigit(line[i + 1][j - offset])) {
+            bottomLeft = atoi(&line[i + 1][j - offset]);
+            offset++;
+          }
+          bottomSum += bottomLeft;
+          bottomLeft = 0;
 
-        // Bottom left
-        offset = 0;
-        while (i != LINE_LENGTH - 1 && !isDigit(line[i + 1][j]) &&
-               isDigit(line[i + 1][j - offset - 1])) {
-          bottomLeft = atoi(&line[i + 1][j - offset - 1]);
-          offset++;
-        }
-
-        // Bottom Right
-        offset = 0;
-        if (i != LINE_LENGTH - 1 && !isDigit(line[i + 1][j]) &&
-            isDigit(line[i + 1][j + offset + 1])) {
-          bottomRight = atoi(&line[i + 1][j + offset + 1]);
-          offset++;
+          // Top right
+          offset = 1;
+          if (isDigit(line[i + 1][j + offset])) {
+            bottomRight += atoi(&line[i + 1][j + offset]);
+            offset++;
+          }
         }
       }
     }
     topSum += top + topLeft + topRight;
     bottomSum += bottom + bottomLeft + bottomRight;
+
     printf("%d %d %d\n", topSum, horizontalSum, bottomSum);
+
     finalAnswer += topSum + horizontalSum + bottomSum;
   }
   printf("Final answer: %d\n", finalAnswer);
