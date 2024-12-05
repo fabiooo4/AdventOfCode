ca4c140059f79f586a8bfc4653ff8da14bc594a9
fabiooo4 <fabioirimie@gmail.com>
2024-12-05 21:39:21 +0100

    completed 2024-day05

--- 2024/rust/src/days/day05.rs
+++ 2024/rust/src/days/day05.rs
@@ -8,26 +8,28 @@ pub fn solve(input: &str) -> SolutionPair {
     let (rules, updates): (HashMap<u64, Vec<u64>>, Vec<Vec<u64>>) = parse_input(input);
 
     let mut sol1: u64 = 0;
-    for update in updates {
+    let mut sol2: u64 = 0;
+
+    for mut update in updates {
         let ordered = is_ordered(&update, &rules);
         if ordered {
-            sol1 += *update.get(update.len()/2).unwrap_or(&0);
+            sol1 += *update.get(update.len() / 2).unwrap_or(&0);
         } else {
-            
+            fix_unordered(&mut update, &rules);
+            sol2 += *update.get(update.len() / 2).unwrap_or(&0);
         }
     }
 
-    let sol2: u64 = 0;
 
     (Solution::from(sol1), Solution::from(sol2))
 }
 
-fn is_ordered(update: &Vec<u64>, rules: &HashMap<u64, Vec<u64>>) -> bool {
+fn is_ordered(update: &[u64], rules: &HashMap<u64, Vec<u64>>) -> bool {
     let mut ordered = true;
     'next_update: for (page_idx, page) in update.iter().enumerate() {
-        for (check_idx,check) in update.clone().iter().enumerate() {
+        for (check_idx, check) in update.iter().enumerate() {
             if let Some(after) = rules.get(page) {
-                if after.contains(check) && check_idx <= page_idx {
+                if after.contains(check) && check_idx < page_idx {
                     ordered = false;
                     break 'next_update;
                 }
@@ -37,19 +39,19 @@ fn is_ordered(update: &Vec<u64>, rules: &HashMap<u64, Vec<u64>>) -> bool {
     ordered
 }
 
-fn fix_unordered(update: &Vec<u64>, rules: &HashMap<u64, Vec<u64>>) -> bool {
-    let mut ordered = true;
-    'next_update: for (page_idx, page) in update.iter().enumerate() {
-        for (check_idx,check) in update.clone().iter().enumerate() {
-            if let Some(after) = rules.get(page) {
-                if after.contains(check) && check_idx <= page_idx {
-                    ordered = false;
-                    break 'next_update;
+fn fix_unordered(update: &mut [u64], rules: &HashMap<u64, Vec<u64>>) {
+    while !is_ordered(update, rules) {
+        let cur_update = Vec::from(&mut *update);
+        for (page_idx, page) in cur_update.iter().enumerate() {
+            for (check_idx, check) in cur_update.iter().enumerate() {
+                if let Some(after) = rules.get(page) {
+                    if after.contains(check) && check_idx < page_idx {
+                        update.swap(check_idx, page_idx);
+                    } 
                 }
             }
         }
     }
-    ordered
 }
 
 fn parse_input(input: &str) -> (HashMap<u64, Vec<u64>>, Vec<Vec<u64>>) {
@@ -197,8 +199,45 @@ mod test {
             75,97,47,61,53
             61,13,29
             97,13,75,29,47";
-        let (p1,p2) = solve(input);
+        let (p1, p2) = solve(input);
         assert_eq!(p1, Solution::from(143_u64));
-        assert_eq!(p2, Solution::from(0_u64));
+        assert_eq!(p2, Solution::from(123_u64));
+    }
+
+    #[test]
+    fn fix_test() {
+        let input = "47|53
+            97|13
+            97|61
+            97|47
+            75|29
+            61|13
+            75|53
+            29|13
+            97|29
+            53|29
+            61|53
+            97|53
+            61|29
+            47|13
+            75|47
+            97|75
+            47|61
+            75|61
+            47|29
+            75|13
+            53|13
+
+            75,47,61,53,29
+            97,61,53,29,13
+            75,29,13
+            75,97,47,61,53
+            61,13,29
+            97,13,75,29,47";
+        let (rules, updates): (HashMap<u64, Vec<u64>>, Vec<Vec<u64>>) = parse_input(input);
+        for mut update in updates {
+            fix_unordered(&mut update, &rules);
+            assert!(is_ordered(&update, &rules));
+        }
     }
 }
