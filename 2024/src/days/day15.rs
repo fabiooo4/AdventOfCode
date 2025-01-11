da581433769cac991554871b1b8b5b563bd47498
fabiooo4 <fabioirimie@gmail.com>
2025-01-11 12:36:44 +0100

    attempted 2024-day15-p2

--- 2024/src/days/day15.rs
+++ 2024/src/days/day15.rs
@@ -1,7 +1,7 @@
 use owo_colors::OwoColorize;
 use std::{
     fmt::Display,
-    ops::{Add, AddAssign},
+    ops::{Add, AddAssign, Mul},
     thread::sleep,
     time::Duration,
 };
@@ -11,25 +11,26 @@ use crate::{Solution, SolutionPair};
 ///////////////////////////////////////////////////////////////////////////////
 
 pub fn solve(input: &str) -> SolutionPair {
-    let (mut grid, directions) = parse_input(input).unwrap_or_else(|e| panic!("{e}"));
+    let (mut small_grid, directions) = parse_input(input).unwrap_or_else(|e| panic!("{e}"));
+    let mut wide_grid = small_grid.to_wide();
 
-    grid.run_robot(&directions);
+    small_grid.run_robot(&directions);
+    let sol1: u64 = small_grid.calculate_gps();
 
-
-    let sol1: u64 = grid.calculate_gps();
-    let sol2: u64 = 0;
+    wide_grid.run_robot(&directions);
+    let sol2: u64 = wide_grid.calculate_gps();
 
     (Solution::from(sol1), Solution::from(sol2))
 }
 
-fn parse_input(input: &str) -> Result<(Grid, Vec<Direction>), String> {
+fn parse_input(input: &str) -> Result<(Grid<SmallBox>, Vec<Direction>), String> {
     let (grid, moves) = match input.trim().split_once("\n\n") {
         Some(val) => val,
         None => return Err(String::from("Wrong input")),
     };
 
     let mut robot: Option<Coordinate<i64>> = None;
-    let mut boxes: Vec<Coordinate<i64>> = Vec::new();
+    let mut boxes: Vec<SmallBox> = Vec::new();
     let mut walls: Vec<Coordinate<i64>> = Vec::new();
     let height = grid.trim().lines().count();
     let width = grid.trim().lines().next().unwrap().trim().chars().count();
@@ -39,7 +40,7 @@ fn parse_input(input: &str) -> Result<(Grid, Vec<Direction>), String> {
             let (x, y) = (x as i64, y as i64);
             match cell {
                 '@' => robot = Some(Coordinate::new(x, y)),
-                'O' => boxes.push(Coordinate::new(x, y)),
+                'O' => boxes.push(SmallBox::new(x, y)),
                 '#' => walls.push(Coordinate::new(x, y)),
                 _ => {}
             }
@@ -61,6 +62,9 @@ fn parse_input(input: &str) -> Result<(Grid, Vec<Direction>), String> {
     Ok((grid, moves))
 }
 
+//
+// Direction
+//
 #[derive(Debug)]
 enum Direction {
     Up,
@@ -111,7 +115,10 @@ impl Display for Direction {
     }
 }
 
-#[derive(Debug, PartialEq, Clone, Copy, Hash, Eq)]
+//
+// Coordinate
+//
+#[derive(Debug, PartialEq, Clone, Copy, Hash, Eq, Default)]
 struct Coordinate<T> {
     pub x: T,
     pub y: T,
@@ -137,6 +144,14 @@ impl<T: Add<Output = T>> Add<Coordinate<T>> for Coordinate<T> {
     }
 }
 
+impl<T: Mul<Output = T>> Mul<(T, T)> for Coordinate<T> {
+    type Output = Self;
+
+    fn mul(self, rhs: (T, T)) -> Self::Output {
+        Coordinate::new(self.x * rhs.0, self.y * rhs.1)
+    }
+}
+
 impl<T> AddAssign<Coordinate<T>> for Coordinate<T>
 where
     T: AddAssign + Add<Output = T> + Copy,
@@ -146,18 +161,133 @@ where
     }
 }
 
-struct Grid {
+//
+// Box
+//
+trait Box {
+    fn position(&self) -> Coordinate<i64>;
+
+    fn display_char() -> char;
+
+    fn move_by(&mut self, amount: Coordinate<i64>);
+
+    fn contains(&self, position: &Coordinate<i64>) -> bool;
+
+    fn size(&self) -> Coordinate<i64>;
+}
+
+//
+// SmallBox
+//
+#[derive(Clone, Copy, PartialEq, Debug, Default)]
+struct SmallBox {
+    position: Coordinate<i64>,
+}
+
+impl SmallBox {
+    fn new(x: i64, y: i64) -> Self {
+        Self {
+            position: Coordinate::new(x, y),
+        }
+    }
+}
+
+impl AddAssign<Coordinate<i64>> for SmallBox {
+    fn add_assign(&mut self, rhs: Coordinate<i64>) {
+        self.position += rhs
+    }
+}
+
+impl Box for SmallBox {
+    fn position(&self) -> Coordinate<i64> {
+        self.position
+    }
+
+    fn display_char() -> char {
+        'O'
+    }
+
+    fn move_by(&mut self, amount: Coordinate<i64>) {
+        *self += amount;
+    }
+
+    fn contains(&self, position: &Coordinate<i64>) -> bool {
+        self.position == *position
+    }
+
+    fn size(&self) -> Coordinate<i64> {
+        Coordinate::new(1, 1)
+    }
+}
+
+//
+// WideBox
+//
+#[derive(Clone, Copy, PartialEq, Debug, Default)]
+struct WideBox {
+    left: Coordinate<i64>,
+    right: Coordinate<i64>,
+}
+
+impl WideBox {
+    fn new(left: Coordinate<i64>, right: Coordinate<i64>) -> Self {
+        Self { left, right }
+    }
+}
+
+impl AddAssign<Coordinate<i64>> for WideBox {
+    fn add_assign(&mut self, rhs: Coordinate<i64>) {
+        self.left += rhs;
+        self.right += rhs;
+    }
+}
+
+impl Box for WideBox {
+    fn position(&self) -> Coordinate<i64> {
+        self.left
+    }
+
+    fn display_char() -> char {
+        '['
+    }
+
+    fn move_by(&mut self, amount: Coordinate<i64>) {
+        *self += amount;
+    }
+
+    fn contains(&self, position: &Coordinate<i64>) -> bool {
+        let left_pos = *position + Direction::Left.delta().into();
+        let right_pos = *position + Direction::Right.delta().into();
+
+        // The box is wide 2 positions, so for each position there can be 2 possible WideBoxes
+        (self.right == *position && self.left == left_pos)
+            || (self.left == *position && self.right == right_pos)
+    }
+
+    fn size(&self) -> Coordinate<i64> {
+        Coordinate::new(2, 1)
+    }
+}
+
+//
+// Grid
+//
+#[derive(Clone)]
+struct Grid<T> {
     robot: Coordinate<i64>,
-    boxes: Vec<Coordinate<i64>>,
+    boxes: Vec<T>,
     walls: Vec<Coordinate<i64>>,
     width: usize,
     height: usize,
 }
 
-impl Grid {
+impl<T> Grid<T>
+where
+    T: PartialEq + Box + Copy + std::fmt::Debug + Default,
+{
     fn new(
         robot: Coordinate<i64>,
-        boxes: Vec<Coordinate<i64>>,
+        boxes: Vec<T>,
         walls: Vec<Coordinate<i64>>,
         width: usize,
         height: usize,
@@ -189,7 +319,7 @@ impl Grid {
         }
 
         // Move all the boxes touching in the same direction
-        if self.boxes.contains(&next_pos) {
+        if self.boxes.iter().any(|b| b.contains(&next_pos)) {
             if let Some(amount) = self.move_boxes(next_pos, direction) {
                 self.robot += amount
             }
@@ -203,7 +333,16 @@ impl Grid {
         position: Coordinate<i64>,
         direction: &Direction,
     ) -> Option<Coordinate<i64>> {
-        let next_pos = position + direction.delta().into();
+        let mut next_pos = position
+            + match self.boxes.first() {
+                Some(b) => b.size() * direction.delta(),
+                None => direction.delta().into(),
+            };
+
+        // Consider the size of the box to calculate the next position
+        if let Some(b) = self.boxes.first() {
+            next_pos += b.size() + (-1, -1).into();
+        }
 
         // Out of bounds check
         if next_pos.x < 0
@@ -219,17 +358,30 @@ impl Grid {
             return None;
         }
 
-        if self.boxes.contains(&next_pos) {
+        if self.boxes.iter().any(|b| b.contains(&next_pos)) {
             // If there is a box next move that box
             if let Some(amount) = self.move_boxes(next_pos, direction) {
-                *self.boxes.iter_mut().find(|&&mut b| b == position).unwrap() += amount;
+                let current_box = self
+                    .boxes
+                    .iter_mut()
+                    .find(|&&mut b| b.contains(&position))
+                    .unwrap();
+
+                // Move the current box by the amount of the next one
+                current_box.move_by(amount);
                 return Some(amount);
             } else {
                 return None;
             }
         } else {
-            *self.boxes.iter_mut().find(|&&mut b| b == position).unwrap() +=
-                direction.delta().into();
+            let current_box = self
+                .boxes
+                .iter_mut()
+                .find(|&&mut b| b.contains(&position))
+                .unwrap();
+
+            // If no box next move the current box by the amount of the direction delta
+            current_box.move_by(direction.delta().into());
         }
 
         Some(direction.delta().into())
@@ -237,35 +389,69 @@ impl Grid {
 
     fn run_robot(&mut self, directions: &[Direction]) {
         for direction in directions {
+            self.move_robot(direction);
             println!("{}[2J", 27 as char);
             println!("{self}");
-            self.move_robot(direction);
-            sleep(Duration::from_millis(3));
+            sleep(Duration::from_millis(100));
         }
     }
 
     fn calculate_gps(&self) -> u64 {
-        self.boxes
-            .iter()
-            .fold(0, |acc, b| acc + 100 * b.y as u64 + b.x as u64)
+        self.boxes.iter().fold(0, |acc, b| {
+            acc + 100 * b.position().y as u64 + b.position().x as u64
+        })
+    }
+
+    fn to_wide(&self) -> Grid<WideBox> {
+        // Double x position and add one more wall to the right
+        let mut new_walls = Vec::with_capacity(self.walls.len());
+        for &wall in &self.walls {
+            new_walls.push(wall * (2, 1));
+            new_walls.push((wall * (2, 1)) + Direction::Right.delta().into());
+        }
+
+        let mut new_boxes = Vec::with_capacity(self.boxes.len());
+        for &small_box in &self.boxes {
+            new_boxes.push(WideBox::new(
+                small_box.position() * (2, 1),
+                (small_box.position() * (2, 1)) + Direction::Right.delta().into(),
+            ));
+        }
+
+        Grid::new(
+            self.robot * (2, 1),
+            new_boxes,
+            new_walls,
+            self.width * 2,
+            self.height,
+        )
     }
 }
 
-impl Display for Grid {
+impl<T: Box> Display for Grid<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let mut grid: Vec<Vec<char>> = vec![vec![' '; self.width]; self.height];
 
         let wall_char = '#';
-        let box_char = 'O';
+        let box_char = T::display_char();
         let robot_char = '@';
 
         for b in &self.boxes {
             let cell = grid
-                .get_mut(b.y as usize)
+                .get_mut(b.position().y as usize)
                 .unwrap()
-                .get_mut(b.x as usize)
+                .get_mut(b.position().x as usize)
                 .unwrap();
             *cell = box_char;
+
+            if box_char == WideBox::display_char() && b.position().x as usize + 1 < self.width {
+                let right_cell = grid
+                    .get_mut(b.position().y as usize)
+                    .unwrap()
+                    .get_mut(b.position().x as usize + 1)
+                    .unwrap();
+                *right_cell = ']';
+            }
         }
 
         for w in &self.walls {
@@ -288,7 +474,7 @@ impl Display for Grid {
             for cell in line {
                 if cell == robot_char {
                     write!(f, "{}", cell.red())?
-                } else if cell == box_char {
+                } else if cell == box_char || cell == ']' {
                     write!(f, "{}", cell.yellow())?
                 } else if cell == wall_char {
                     write!(f, "{}", cell.white())?
@@ -313,5 +499,35 @@ mod test {
             Coordinate::new(1, 1) + Coordinate::new(1, 1),
             Coordinate::new(2, 2)
         )
+    }
+
+    #[test]
+    fn move_by_test() {
+        let mut sb = SmallBox::new(0, 0);
+        sb.move_by(Direction::Right.delta().into());
+        assert_eq!(SmallBox::new(1, 0), sb);
+    }
+
+    #[test]
+    fn small_box_contains() {
+        let sb = SmallBox::new(0, 0);
+        assert!(sb.contains(&Coordinate::new(0, 0)));
+
+        let wb = WideBox::new((0, 0).into(), (1, 0).into());
+        assert!(wb.contains(&Coordinate::new(1, 0)));
+    }
+
+    #[test]
+    fn find_wide_box() {
+        let boxes = [
+            WideBox::new((0, 0).into(), (0, 1).into()),
+            WideBox::new((2, 2).into(), (3, 2).into()),
+        ];
+        let position = Coordinate::new(2, 2);
+        assert!(!boxes[0].contains(&position));
+        assert!(boxes[1].contains(&position));
+
+        let b = boxes.iter().find(|b| b.contains(&position));
+        assert_eq!(b, Some(&WideBox::new((2, 2).into(), (3, 2).into())));
     }
 }
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <!-- AOC TILES BEGIN -->
 <h1 align="center">
-  2024 - 28 ⭐ - Rust
+  2024 - 29 ⭐ - Rust
 </h1>
 <a href="2024/src/days/day01.rs">
   <img src=".aoc_tiles/tiles/2024/01.png" width="161px">
@@ -43,6 +43,9 @@
 </a>
 <a href="2024/src/days/day14.rs">
   <img src=".aoc_tiles/tiles/2024/14.png" width="161px">
+</a>
+<a href="2024/src/days/day15.rs">
+  <img src=".aoc_tiles/tiles/2024/15.png" width="161px">
 </a>
 <h1 align="center">
   2023 - 7 ⭐ - C
